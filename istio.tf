f9b3641e470f9140c11e1950a2e4f7995b8a0bd8
Samuel Dumont <samuel@dataroots.io>
2020-06-09 16:33:04 +0200

    Adding the proper settings for the module passing

--- istio.tf
+++ istio.tf
@@ -1,4 +1,9 @@
 module "istio" {
+  providers = {
+    kubernetes = kubernetes
+    k8s        = k8s
+    helm       = helm
+  }
   source                   = "./istio"
   istio_namespace          = var.istio_namespace
   istio_operator_namespace = var.istio_operator_namespace
