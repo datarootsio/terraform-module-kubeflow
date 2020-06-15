42baee7cd5688b46c7045d137fe9a25d56995a4a
Samuel Dumont <samuel@dataroots.io>
2020-06-15 13:31:47 +0200

    Fixing the format

--- kubeflow.tf
+++ kubeflow.tf
@@ -3,8 +3,8 @@ module "kubeflow" {
     kubernetes = kubernetes
     k8s        = k8s
   }
-  source = "./kubeflow"
-  domain_name = var.domain_name
+  source           = "./kubeflow"
+  domain_name      = var.domain_name
   use_cert_manager = module.istio.use_cert_manager
   certificate_name = module.istio.certificate_name
 }
