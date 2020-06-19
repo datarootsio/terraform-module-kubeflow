4350ab301c78a2d5048fdf133ab847f85bfaebac
Samuel Dumont <samuel@dataroots.io>
2020-06-19 08:21:18 +0200

    Adding the ingress

--- kubeflow.tf
+++ kubeflow.tf
@@ -23,3 +23,25 @@ resource "k8s_manifest" "kubeflow_kfdef" {
     { namespace = kubernetes_namespace.kubeflow.metadata.0.name }
   )
 }
+
+locals {
+  kubeflow_ingress_vs_manifests = split("\n---\n", templatefile(
+    "${path.module}/manifests/kubeflow/gateway-vs.yaml",
+    {
+      credential_name  = var.certificate_name,
+      domain_name      = var.domain_name,
+      istio_namespace  = var.istio_namespace
+      namespace        = kubernetes_namespace.kubeflow.metadata.0.name
+      use_cert_manager = var.use_cert_manager
+    }
+    )
+  )
+}
+
+resource "k8s_manifest" "centraldashboard_application_vs" {
+  depends_on = [kubernetes_deployment.kubeflow_operator]
+  count      = length(local.kubeflow_ingress_vs_manifests)
+  content    = local.kubeflow_ingress_vs_manifests[count.index]
+}
+
+
