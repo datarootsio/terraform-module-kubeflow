0c63e69bc57a38114cfbab41980585aaed0c5f1b
Samuel Dumont <samuel@dataroots.io>
2020-06-26 09:12:11 +0200

    Adding the initial changes to make this compatible with tf 0.13

---
+++ versions.tf
@@ -0,0 +1,19 @@
+terraform {
+  required_providers {
+    helm = {
+      source = "hashicorp/helm"
+    }
+    k8s = {
+      source = "registry.terraform.local/banzaicloud/k8s"
+      version = "0.7.7"
+    } 
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+    }
+    null = {
+      source = "hashicorp/null"
+    }
+  }
+  required_version = ">= 0.13"
+}
