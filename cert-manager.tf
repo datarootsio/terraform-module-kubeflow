40c14eb7c11a143ce5257792fcfa5785cf738aa7
Samuel Dumont <samuel@dataroots.io>
2020-08-24 16:10:18 +0200

    Adding a dirty trick to wait for certs to be ready

--- cert-manager.tf
+++ cert-manager.tf
@@ -24,8 +24,20 @@ resource "helm_release" "cert_manager" {
   }
 }
 
-resource "k8s_manifest" "selfsigned_issuer" {
+resource "null_resource" "wait_for_certs" {
+  count = var.install_cert_manager ? 1 : 0
   depends_on = [helm_release.cert_manager]
+  triggers = {
+    always_run = timestamp()
+  }
+
+  provisioner "local-exec" {
+    interpreter = ["/bin/bash", "-c"]
+    command     = "sleep 180" }
+}
+
+resource "k8s_manifest" "selfsigned_issuer" {
+  depends_on = [null_resource.wait_for_certs]
   content = templatefile(
     "${path.module}/manifests/cert-manager/self-signed.yaml",
     {}
@@ -34,7 +46,7 @@ resource "k8s_manifest" "selfsigned_issuer" {
 
 resource "k8s_manifest" "letsencrypt_issuer" {
   count      = var.use_cert_manager ? 1 : 0
-  depends_on = [helm_release.cert_manager]
+  depends_on = [null_resource.wait_for_certs]
   content = templatefile(
     "${path.module}/manifests/cert-manager/letsencrypt-prod.yaml",
     {
